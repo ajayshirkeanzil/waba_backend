8365fbb5791ba0885711f0db3f2c06e1e17c977d
ajayshirkeanzil <ajay.shrike@anzilsoft.com>
2025-05-26 12:11:44 +0530

    Db Changes

--- db.connection.js
+++ db.connection.js
@@ -1,22 +1,22 @@
 import mariadb from "mariadb"
 
 const pool = mariadb.createPool({
-  port: 3307,
-  host: "localhost",
-  user: "root",
-  password: "root",
-  database: "whatsapp-business-portfolio",
+  // port: 3307,
+  // host: "localhost",
+  // user: "root",
+  // password: "root",
+  // database: "whatsapp-business-portfolio",
   waitForConnections: true,
   connectionLimit: 10,
   idleTimeoutMillis: 30000,
   supportBigNumbers: true,
   bigNumberStrings: true,
  
-  // host: "ec2-3-108-102-118.ap-south-1.compute.amazonaws.com",
-  // user: "root",
-  // password: "AnzilChatbot!2025",
-  // database: "whatsapp-business-portfolio",
-  // port: 3306,
+  host: "ec2-3-108-102-118.ap-south-1.compute.amazonaws.com",
+  user: "waba_admin",
+  password: "b93de6e4-58d8-4dac-b7a7-8a71667eb41a",
+  database: "whatsapp-business-portfolio",
+  port: 3306,
 });
 
 export default pool;
